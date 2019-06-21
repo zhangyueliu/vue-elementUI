bb5e1a4fd97cdcfe205aa107db300fb204de6f5a
zhangyue <2698051674@qq.com>
2019-06-21 17:06:03 +0800

    修改flex页面讲解的内容

--- src/pages/functions/pagesDemo/FlexBoxTest.vue
+++ src/pages/functions/pagesDemo/FlexBoxTest.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="flex-box">
+  <div class="flex-box" id="demo-flex">
     <div class="flex-item">
-      <span class="one-ellipsis">这是第一个flex项目,在这里显示一行，实现内容过长，出现省略号的效果</span>
+      <span class="one-ellipsis info">这是第一个flex项目,在这里显示一行，实现内容过长，出现省略号的效果</span>
     </div>
 
-    <div class="flex-item adapt-flex">
-      <span>这是第二个flex项目，实现弹性高度</span>
+    <div class="flex-item adapt-flex main-box">
+      <span class="info">这是第二个flex项目，实现弹性高度</span>
+      <a class="info link" href="http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html" target="_blank">Flex 布局教程：语法篇</a>
+      <a class="info link" href="http://www.ruanyifeng.com/blog/2015/07/flex-examples.html" target="_blank">Flex 布局教程：实例篇</a>
+      <span class="">css权重</span>
+      <img src="../../../assets/img/weight.jpg" class="img"/>
     </div>
     <div class="flex-item">
-      <span class="two-ellipsis">这是第三个flex项目,在这里显示两行，实现内容过长，出现省略号的效果，多加点内容文字，超出两行，超出两行，超出两行</span>
+      <span class="two-ellipsis info">这是第三个flex项目,在这里显示两行，实现内容过长，出现省略号的效果，多加点内容文字，超出两行，超出两行，超出两行</span>
     </div>
   </div>
 </template>
@@ -20,17 +24,23 @@
 </script>
 
 <style scoped>
-.flex-box{
-  width: 400px;
-  height:100%;
-  background-color: #cccccc;
-  display: flex;
-  flex-direction: column;
-}
+  .flex-box{
+    width: 400px;
+    height:100%;
+    background-color: #cccccc;
+    display: flex;
+    flex-direction: column;
+  }
   .flex-item{
-    background-color: antiquewhite;
+    background-color: gray;
     padding: 8px;
-    border:1px solid #cccccc;
+    border:1px solid #dddddd;
+  }
+  .img{
+    width: 100%;
+  }
+  .link{
+    font-weight: bolder;
   }
   .adapt-flex{
     flex: 1;
@@ -48,5 +58,21 @@
     line-height: 20px;
     height: 40px;
     overflow: hidden;
+  }
+  /*css权重*/
+  .info{
+    font-family: 楷体;
+    font-size: 20px;
+    color: red;
+    display: block;
+  }
+  .main-box .info{
+    color: blue;
+  }
+  #demo-flex .info{
+    color: purple;
+  }
+  #demo-flex .main-box .info{
+    color: brown;
   }
 </style>
